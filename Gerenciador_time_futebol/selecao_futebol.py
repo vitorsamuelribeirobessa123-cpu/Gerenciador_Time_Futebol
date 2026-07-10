d98cd697efd16bdb7b2bae68d97be14057887e55
Samuel Bessa <vitorsamuelribeirobessa123@gmail.com>
2026-07-10 15:41:56 -0300

    Refactor Selecao class with improved comments
    Updated comments to clarify attribute usage and methods.

--- Gerenciador_time_futebol/selecao_futebol.py
+++ Gerenciador_time_futebol/selecao_futebol.py
@@ -2,7 +2,7 @@ class Selecao:
     def __init__(self, nome_selecao):
         self.__nome_selecao = nome_selecao
         self.__jogadores = []
-
+    '''Os atribuots nome_seleção e jogadores são instanciados e sofrem name mangling (adiciona-se o nome da classe no do atributo.), mas são recuperados pelos properties.'''
     @property
     def nome_selecao(self):
         return self.__nome_selecao
@@ -10,7 +10,8 @@ class Selecao:
     @property
     def jogadores(self):
         return self.__jogadores
-    
+    '''a lista jogadores é manipulada com adição (contratar e colocar_jogador_titular) e retirada (demitir) de jogadores, e printada (listar_jogadores).
+    E é printada também a comissão (listar_comissão)'''
     def contratar(self,novo_membro):
         self.__jogadores.append(novo_membro)
 
